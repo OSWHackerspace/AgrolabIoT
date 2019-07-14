d40d262ce397a20f0395e8d489a4ea30bd65e1d1
smorcuend <smorcuend@gmail.com>
2019-07-14 11:45:03 +0200

    some comments

--- src/config.tmpl.h
+++ src/config.tmpl.h
@@ -12,9 +12,23 @@
  * Source Code: https://github.com/OSWHackerspace/AgrolabIoT 
  * Community: https://github.com/osweekends
 */
+
+/* ***** This file must be renamed to "config.h" according your project credentials ****/
+/* For security reasons, "config.h" is ignored by .gitignore */
+
+/* Modify these variables to connect to Thinger.io platform
+  USERNAME: Put here your user name in the IoT platform thinger.io
+  DEVICE_ID: Put here the ID of the ESP8266 device in the thinger.io platform
+  DEVICE_CREDENTIAL: Put here the ID of the ESP8266 device in the thinger.io platform
+*/
 #define USERNAME ""
 #define DEVICE_ID ""
 #define DEVICE_CREDENTIAL ""
 
+/* Modify these two variables to connect to your local wi-fi
+  SSID_1: Name of the network to connect to
+  SSID_PASSWORD_1: Password to access the network
+*/
 #define SSID_1 ""
 #define SSID_PASSWORD_1 ""
+
